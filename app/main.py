a81463050b492d2c095f02d30cfceff574cca24e
KD-Zefan <kd.zefan@gmail.com>
2024-09-09 22:15:38 -0400

    removed some files

--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from app.api.v1.endpoints import auth, users
 app = FastAPI()
 
 # Include routers
-app.include_router(auth.router, prefix="/auth", tags=["auth"])
-app.include_router(users.router, prefix="/users", tags=["users"])
 
 
 @app.get("/health-check")
